e90a1411bc7b384dce5677b4760712514962fc93
zimmchad <zimmermanchad@ymail.com>
2022-06-09 19:17:22 -0400

    third commit

--- src/main/java/org/wecancoeit/reviews/repo/BandRepository.java
+++ src/main/java/org/wecancoeit/reviews/repo/BandRepository.java
@@ -12,17 +12,17 @@ public class BandRepository {
     HashMap<Long, Band> bandList = new HashMap<>();
 
 
-    Band bandA = new Band(1L,"Pearl Jam", "HOF", "https://pearljam.com");
-    Band bandB = new Band(2L,"Nirvana", "HOF", "https://nirvana.com");
-    Band bandC = new Band(3L,"Soundgarden", "HOF", "https://soundgardenworld.com");
-    Band bandD = new Band(4L,"Screaming Trees", "One Hit Wonders", "https://en.wikipedia.org/wiki/Screaming_Trees_discography");
-    Band bandE = new Band(5L,"Dinosaur Jr.", "One Hit Wonders", "https://dinosaurjr.com");
-    Band bandF = new Band(6L,"Mother Love Bone", "Founding Fathers", "https://en.wikipedia.org/wiki/Mother_Love_Bone");
-    Band bandG = new Band(7L,"Mudhoney", "Founding Fathers", "https://mudhoneyonline.com");
-    Band bandH = new Band(8L,"Love Battery", "How were they not famous?", "https://www.eskimo.com/~tracyr/LoveBat.html");
-    Band bandI = new Band(9L,"The Gits", "How were they not famous", "https://en.wikipedia.org/wiki/The_Gits");
-    Band bandJ = new Band(10L,"L7", "One Hit Wonders","https://www.facebook.com/L7theband");
-    Band bandK = new Band(11L,"Alice in Chains", "HOF", "https://tour.aliceinchains.com");
+    Band bandA = new Band(1L,"Pearl Jam", "Hall of Fame.  'Ten' stayed on the charts for 5 years", "https://pearljam.com");
+    Band bandB = new Band(2L,"Nirvana", "Hall of Fame.  Sold over 75 million records", "https://nirvana.com");
+    Band bandC = new Band(3L,"Soundgarden", "Rock Gods with 3 No. 1 and 4 top 10 billboard hits", "https://soundgardenworld.com");
+    Band bandD = new Band(4L,"Screaming Trees", "'Nearly Lost You' reached #5 on the billboard charts", "https://en.wikipedia.org/wiki/Screaming_Trees_discography");
+    Band bandE = new Band(5L,"Dinosaur Jr.", "'Feel The Pain' was their only hit, but what an injustice", "https://dinosaurjr.com");
+    Band bandF = new Band(6L,"Mother Love Bone", "Founding Fathers, 'Chloe/Dancer' is their biggest hit", "https://en.wikipedia.org/wiki/Mother_Love_Bone");
+    Band bandG = new Band(7L,"Mudhoney", "Founding Fathers, 'Overblown' is their biggest hit", "https://mudhoneyonline.com");
+    Band bandH = new Band(8L,"Love Battery", "How were they not famous?  Incredible, polished songs", "https://www.eskimo.com/~tracyr/LoveBat.html");
+    Band bandI = new Band(9L,"The Gits", "Female lead vocal.  Ultimate mosh pit/head-banging grunge", "https://en.wikipedia.org/wiki/The_Gits");
+    Band bandJ = new Band(10L,"L7", "Another leading lady.  Had a big hit with 'Shit-list'","https://www.facebook.com/L7theband");
+    Band bandK = new Band(11L,"Alice in Chains", "18 top 10 songs and 5 No. 1 hits", "https://tour.aliceinchains.com");
     Band bandL = new Band(12L,"Hammerbox", "Underground", "https://hammerbox.bandcamp.com");
     Band bandM = new Band(13L,"Supersuckers", "Underground", "https://supersuckers.com");
     Band bandN = new Band(14L,"Seaweed", "Underground", "https://seaweed.bandcamp.com");
